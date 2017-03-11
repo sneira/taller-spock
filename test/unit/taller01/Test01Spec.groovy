12bb87dfff5b5d1e29fb747a9980325659cc2ab3
sofia <sofia@virtualsw.com>
2017-03-11 14:34:42 +0100

    Proyecto Grails y test inicial de prueba

---
+++ test/unit/taller01/Test01Spec.groovy
@@ -0,0 +1,14 @@
+package taller01
+
+import spock.lang.Specification
+
+/**
+ * Created by sofia on 11/03/17
+ */
+class Test01Spec extends Specification {
+
+    def "Prueba"() {
+        expect:
+        1 > 0
+    }
+}
